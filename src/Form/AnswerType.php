ea8e1be6df98ffe9672a0efc26d05954c288610e
vladsolntsev <vlad.solnt@gmail.com>
2021-02-03 11:00:56 +0100

    crud bootstrap and fontawesome ok

---
+++ src/Form/AnswerType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Answer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnswerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text')
+            ->add('type')
+            ->add('rating')
+            ->add('question')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Answer::class,
+        ]);
+    }
+}
